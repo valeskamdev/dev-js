3f28c60af5cb92d9f411cba5419892f5dc8e4e59
valeskamdev <valeskkitty@gmail.com>
2023-01-26 00:38:59 -0300

    even or odd

---
+++ challenge1/evenOdd.js
@@ -0,0 +1,9 @@
+// Faça um programa onde leia um numero e diga se ele é par ou impar.
+
+const number = 128;
+
+if (number % 2 === 0) {
+    console.log(`${number} is even`);
+} else {
+    console.log(`${number} is odd`);
+}

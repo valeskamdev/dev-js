73e09f5f9afd789cf703529adcf7453f692f4dc9
valeskamdev <valeskkitty@gmail.com>
2023-01-30 17:02:02 -0300

    exact time

--- challenge2/exactTime.js
+++ challenge2/exactTime.js
@@ -1,8 +1,7 @@
 // Escreva um programa onde, você chame uma função, e ela diga a hora exata!
 
 const exactTime = () => {
-    const now = new Date();
-    return `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    return new Date().toLocaleTimeString('pt-BR');
 }
 
-console.log(exactTime()); // 21:56:35  (my exact time)
+console.log(exactTime()); // current time  xx:xx:xx
